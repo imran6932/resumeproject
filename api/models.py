2c286c91397792963a6a20608821939f6500c401
Ubuntu <ubuntu@ip-172-31-12-195.ap-south-1.compute.internal>
2022-01-07 06:11:20 +0000

    bugs fixed

--- api/models.py
+++ api/models.py
@@ -7,9 +7,10 @@ from django.db.models.fields import FloatField
 class my_input(models.Model):
     upload_file = models.FileField(upload_to='my_files')
 
-    '''def __str__(self):
-        return self.upload_file'''
-    
+
+
+class image(models.Model):
+    image_from_pdf = models.ImageField( upload_to="image_fldr")
      
     
      
--- api/views.py
+++ api/views.py
@@ -14,15 +14,11 @@ from .models import my_input
 from .serializers import MyInputSerializer
 
 from pyresparser import ResumeParser
-import pprint
 import json
 
-import pdf2image
-import easyocr as e
-import numpy as np
-import PIL
-from PIL import ImageDraw
+import pdfplumber
 import spacy 
+import glob, sys, fitz
 
 
 import random
@@ -52,25 +48,23 @@ class ResumeAPI(CreateAPIView):
     Other = []
     links = []
     DOB=[]
-    
-    reader = e.Reader(['en'])
-    images = (pdf2image.convert_from_path(req_file, 
-      poppler_path=r'poppler-0.68.0\bin'))
-      
-    for i in range(0,len(images)):
-        bounds.append(reader.readtext(np.array(images[i]), min_size=0,
-          slope_ths=0.2,ycenter_ths=0.7,height_ths=0.6,width_ths=0.8,
-          decoder='beamsearch', beamWidth=10))
-    text=''
-    for j in range(len(bounds)):
-          for i in range(len(bounds[j])):
-              text = text + bounds[j][i][1] + '\n'
-       
+
+    all_text=''
+
+    with pdfplumber.open(req_file) as pdf:
+
+      for page in pdf.pages:
+
+        single_pg_txt = page.extract_text()
+
+        all_text = all_text + '\n' + single_pg_txt
+
+
 
     nlp2 = spacy.load(r'en_core_web_sm')
     
    
-    data = text 
+    data = all_text
     
     doc2 = nlp2(data)
     for ent in doc2.ents:
@@ -217,136 +211,15 @@ class ResumeAPI(CreateAPIView):
   
 
 
-'''def index(request):
-  return render(request,'index.html') ''' 
-
-# def result(request):
-#   fileobj = request.FILES['test']
-#   my_input.objects.create(upload_file=fileobj)
-#   user_input = my_input.objects.all().last()
-#   req_file = user_input.upload_file.path
-  
-#   bounds= []
-#   Name = []
-#   Work = []
-#   Skills = []
-#   Education = []
-#   Projects = []
-#   Certificates = []
-#   Achievements = []
-#   Projects = []
-#   Other = []
-  
-#   reader = e.Reader(['en'])
-#   images = (pdf2image.convert_from_path(req_file, 
-#     poppler_path=r'C:\Program Files\poppler-0.68.0\bin'))
-    
-#   for i in range(0,len(images)):
-#       bounds.append(reader.readtext(np.array(images[i]), min_size=0,
-#         slope_ths=0.2,ycenter_ths=0.7,height_ths=0.6,width_ths=0.8,
-#         decoder='beamsearch', beamWidth=10))
-#   text=''
-#   for j in range(len(bounds)):
-#         for i in range(len(bounds[j])):
-#             text = text + bounds[j][i][1] + ' '
-
-#   nlp2 = spacy.load(r"C:\Users\Asus\OneDrive\Desktop\Practice Selenium\OCR and NER")
-#   data = text
-  
-#   doc2 = nlp2(data)
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Name':
-#       Name.append(ent.text_with_ws)
-#     #print("NAME: ",Name )        
-
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Work Exp':
-#       Work.append(ent.text_with_ws)
-#     #print("WORK EXPERIENCE: ",Work)        
-
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Education':
-#       Education.append(ent.text_with_ws)
-#     #print("EDUCATION: ",Education)        
-
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Projects':
-#       Projects.append(ent.text_with_ws)
-#     #print("PROJECTS: ",Projects)        
-
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Skills':
-#       Skills.append(ent.text_with_ws)
-#     #print("SKILLS: ",Skills)        
-
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Certificates':
-#       Certificates.append(ent.text_with_ws)
-#     #print("CERTIFICATES: ",Certificates)        
-
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Achievements':
-#       Achievements.append(ent.text_with_ws)
-#     #print("ACHIEVEMENTS: ",Achievements)        
-
-#   for ent in doc2.ents:
-#     if ent.label_ == 'Other Deatils':
-#       Other.append(ent.text_with_ws)
-#     #print("OTHER DETAILS: ",Other)        
-
-
-        
-#   import re
-#   phone_pattern = re.compile(r'[0-9]{10}')
-#   phone_matches = phone_pattern.findall(data)
-#     #print("CONTACT: ",phone_matches)
-
-#   email_pattern = re.compile(r'[a-zA-Z0-9-\.]+@[a-zA-z-0-9-\.]*\.[a-z]*')
-#   email_matches = email_pattern.findall(data)
-#     #print("EMAIL: ",email_matches)
-
-#   status = re.compile(r'\bSingle\b | \bMarried\b | \bUnmarried\b | \bDivorced\b ', flags=re.I | re.X)
-#   status =  status.findall(data)
-#     #print("MARITAL STATUS: ",status)
-
-#   nationality = re.compile(r'\bIndia\b | \bIndian\b | \bNRI\b | \bNon-Indian\b ', flags=re.I | re.X)
-#   nationality = nationality.findall(data)
-#     #print("NATIONALITY: ",nationality)
-
-#   sex = re.compile(r'\bMale\b | \bFemale\b | \bLGBT\b | \bLGBTQ\b | \bTransGender | \bTrans Gender\b | \bTrans-Gender\b | \bGay\b | \bLesbian\b', flags=re.I | re.X)
-#   sex = sex.findall(data)
-#     #print("GENDER: ",sex)
-
-#   languages = re.compile(r'\bHindi\b | \bEnglish\b | \bTamil\b | \bGujrati\b | \bTelugu\b | \bMalayalam\b | \bKannad\b | \bBengali\b | \bMarathi\b | \bPunjabi\b | \bKashmiri\b | \bBihari\b | \bUrdu\b | \bArabic\b | \bAssami\b | \bManipuri\b | \bGoan\b | \bNepali\b', flags=re.I | re.X)
-#   languages= languages.findall(data)
-#     #print("LANGUAGES KNOWN: ",languages)
-
-
-#   Final_result = {
-#         "NAME: ":Name,
-#         "CONTACT: ":phone_matches,
-#         "EMAIL: ":email_matches,
-#         "WORK EXPERIENCE: ":Work,
-#         "EDUCATION: ":Education,
-#         "PROJECTS: ":Projects,
-#         "SKILLS: ":Skills,
-#         "CERTIFICATES: ":Certificates,
-#         "ACHIEVEMENTS: ":Achievements,
-#         "LANGUAGES KNOWN: ":languages,
-#         "MARITAL STATUS: ":status,
-#         "NATIONALITY: ":nationality,
-#         "GENDER: ":sex,
-#         "LANGUAGES KNOWN: ":languages,
-#         "OTHER DETAILS: ":Other
-#     }  
-#   print(Final_result) 
-#   return render(request, 'index.html',Final_result)
-  
-  
-
-'''def give_file():
-
-  user_input = my_input.objects.all().last()
-  req_file = user_input.upload_file
-  return req_file'''
-
+# pdf  =  fitz.open(req_file)
+    # image_list = pdf.getPageImageList(0)
+    # for image in image_list:
+    #   xref = image[0]
+    #   pix = fitz.Pixmap(pdf,xref)
+    #   if pix.n<5:
+    #       pix.writePNG(f'{xref}.png')
+    #   else:
+    #     pix1 = fitz.open(fitz.csRGB,pix)
+    #     pix1.writeePNG(f'{xref}.png')
+    #     pix1
+    #   pix = None
